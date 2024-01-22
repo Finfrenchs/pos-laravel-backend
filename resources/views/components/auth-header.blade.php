a3eaa44c0083e48b065e71fc3a8c6ed66ea86867
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2024-01-22 21:25:00 +0700

    add OrderController api, order model, order migration

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,6 +1,7 @@
  <div class="login-brand">
-     <img src="{{ asset('img/stisla-fill.svg') }}"
+     <img src="{{ asset('img/sruput-glek.png') }}"
          alt="logo"
-         width="100"
+         width="130"
+         height="130"
          class="shadow-light rounded-circle">
  </div>
